99e5841a8ed9a1ea3d557c00502e5726894b233f
Nutan Nara <nutan@ncubeschool.org>
2016-02-18 18:06:48 +0530

    Update: Log

--- classes/Log.php
+++ classes/Log.php
@@ -1,6 +1,13 @@
 <?php
 class Log {
     public function error($type, $error) {
-        file_put_contents('logs/error.log',  date('F j Y h:i:s A') . ' - ' . strtoupper($type) . ' - ' . $error . "\n", FILE_APPEND);
+        $type = strtoupper($type);
+        
+        switch ($type) {
+            case 'DB':
+                $error = $error->getFile().'('.$error->getLine().')'.' ==>'.$error->getMessage();
+                break;
+        }
+        file_put_contents('logs/error.log',  date('F j Y h:i:s A') . ' - ' . $type . ' - ' . $error . "\n", FILE_APPEND);
     }
 }
