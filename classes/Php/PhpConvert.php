24f76d843990080a575340d1ad7d8a9880d64542
Nutan Nara <nutan@ncubeschool.org>
2016-03-22 02:21:35 +0530

    BugFix: Skip conversion of non-object to array in PhpConvert toArray function

--- classes/Php/PhpConvert.php
+++ classes/Php/PhpConvert.php
@@ -1,9 +1,12 @@
-<?php
+<?php 
 class PhpConvert {
     public function toArray($data) {
         $data = (array) $data;
         array_walk_recursive($data, function(&$value) {
-            $value = (array) $value; 
+            $type = gettype($value);
+            if ($type === 'object') {
+                $value = (array) $value;
+            }
         });
         return $data;
     }
